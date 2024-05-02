e70d705dbfc175856eb35422047c8ee84d4c1447
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-05-02 08:37:42 -0400

    Add user model to mongo-database exports in index.ts

---
+++ src/data/mongo/models/user.model.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+// El schema es como luce nuestra entidad
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [ true, 'Name is required' ]
+    },
+    email: {
+        type: String,
+        require: [ true, 'Email is required' ],
+        unique: true
+    },
+    password: {
+        type: String,
+        require: [ true, 'Password is required' ]
+    },
+    img: {
+        type: String,
+    },
+    role: {
+        type: [String],
+        default: [ 'USER_ROLE' ],
+        enum: [ 'ADMIN_ROLE', 'USER_ROLE' ],
+    }
+} );
+
+export const UserModel = mongoose.model('User', userSchema);
